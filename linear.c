9b1786caefb6a5a1f6e04f30e0748235448386e7
shubham999sh <shubhamsharmavks@gmail.com>
2019-07-08 10:09:21 +0530

    First Commit

---
+++ linear.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+ int i,key,first,last,a[10],n,r;
+first = a[i];
+printf("enter the numbert of elements in the array:");
+scanf("%d",&n);
+ printf("enter the array:");
+ scanf("%d",&a[i]);
+ printf("enter the no which you want to find:");
+ scanf("%d",&key);
+ for(i=a[i];i<=n;i++)
+  {
+	if(key==a[i])
+	{
+		printf("***number succesfully find***");
+	}
+	printf("%d",i);
+  }
+ return 0;
+}
